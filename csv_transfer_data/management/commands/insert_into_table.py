b62e36fabba68d847cb28072d439cd5d7404dec1
andrey_mazo <andreymazo@mail.ru>
2023-09-03 14:55:54 +0300

    insert into table with mem profiler

--- csv_transfer_data/management/commands/insert_into_table.py
+++ csv_transfer_data/management/commands/insert_into_table.py
@@ -1,37 +1,11 @@
+import os
+import memory_profiler
 import jsonpickle
 import csv
 import json
 import sqlite3
 from django.core.management import BaseCommand
-
-
-def write_rows(nomer, data_for_save):
-    with open(f'newfile{nomer}', 'w+') as f:
-        # data_for_save = []
-        # data_for_save.append(ii)
-        #####################################
-        # writer = csv.writer(f)
-        # writer.writerow(data_for_save)
-        f.writelines(data_for_save)
-
-def devider():
-        filename = 'server.csv'
-        rows_infile = 5000
-        f = open(filename)
-        header = next(f)
-        data_for_save = [header]
-        index = 0
-        numer = 0
-        for i in f:
-            if index >= rows_infile:
-                index = 0
-                numer += 1
-                write_rows(numer, data_for_save)
-                data_for_save = [header]
-            data_for_save.append(i)
-            index += 1
-        # numer += 1
-            
+from csv_transfer_data.management.commands.divider import devider
             
 
 class Command(BaseCommand):
@@ -44,50 +18,49 @@ class Command(BaseCommand):
 # json_server поле json из server.csv
 # json_client поле json из client.csv
     
-    
+    @memory_profiler.profile
     def handle(self, *args, **options):
-        devider()
-        
-        
+        filename1 = 'new/newfile1'
+        if not os.path.isfile(filename1):
+            devider(filename = 'server.csv', rows_infile = 5000)
+
+        con = sqlite3.connect("my_db.db", timeout=40)
+        con.isolation_level = None
+        ############################################################
+            # cur = con.cursor()
+        # con.execute("CREATE TABLE first(timestamp TEXT, player_id INT, event_id TEXT, error_id TEXT, json_server TEXT, json_client TEXT, year TEXT, score TEXT)")
+# https://ruseller.com/lessons.php?id=2277
 
 
-        # con = sqlite3.connect("my_db.db", timeout=40)
-        # con.isolation_level = None
-#         ############################################################
-#             # cur = con.cursor()
-#         # con.execute("CREATE TABLE first(timestamp TEXT, player_id INT, event_id TEXT, error_id TEXT, json_server TEXT, json_client TEXT, year TEXT, score TEXT)")
-# # https://ruseller.com/lessons.php?id=2277
+        # con.execute('''CREATE TABLE first
+        #     (timestamp     TEXT     NOT NULL,
+        #     player_id           TEXT    NOT NULL,
+        #     event_id            INT     NOT NULL,
+        #     error_id     TEXT,   
+        #     json_server TEXT ,
+        #     json_client  TEXT ,    
+        #     year     TEXT  ,
+        #     score    TEXT    ;''')
+        # con.commit()
+        # con.close()
+            # , player_id, , i[1], error_id, i[3], json_client, i[5],, year, score, i[6], i[7], i[8], i[9] timestamp, json_server
+        for i in range(1, len(os.listdir('new'))):
+            with open(f'new/newfile{i}', 'r') as f:
+                next(f)
+                reader1 = csv.reader(f)
+                for i in reader1:
+                    # print(i[0])
 
-
-#         # con.execute('''CREATE TABLE first
-#         #     (timestamp     TEXT     NOT NULL,
-#         #     player_id           TEXT    NOT NULL,
-#         #     event_id            INT     NOT NULL,
-#         #     error_id     TEXT,   
-#         #     json_server TEXT ,
-#         #     json_client  TEXT ,    
-#         #     year     TEXT  ,
-#         #     score    TEXT    ;''')
-#         # con.commit()
-#         # con.close()
-#             # , player_id, , i[1], error_id, i[3], json_client, i[5],, year, score, i[6], i[7], i[8], i[9] timestamp, json_server
-        
-#         with open('server.csv', 'r') as f:
-#             next(f)
-#             reader1 = csv.reader(f)
-#             for i in reader1:
-#                 # print(i[0])
-
-#                 con.execute(f"INSERT INTO first ('timestamp', 'json_server') VALUES (?, ?)", [str({i[0]}), jsonpickle.encode({i[3]})])
-# # #1612068364,76,38152c64-d439-43cf-9856-4ca35d39d7ac,"{""purpose"": ""...
-# #         # result = cur.execute("select * from cheaters")
-# #         # data_from_cheaters = [i for i in result]
-# #         #############################################################
-# #         # data_from_cheaters_0 = [i[0] for i in result]
-# #         # print('len(data_from_cheaters)', len(data_from_cheaters))
-# #         # cur.execute("CREATE TABLE first(timestamp, player_id, event_id, error_id, json_server, json_client, year, score)")
-# #         # list(cur.fetchall())
-# #         # print(data_from_cheaters)#[...(999892, '2021-01-25 14:38:15'), (999922, '2021-05-18 03:50:08')]
-# #         # print(data_from_cheaters_0)#[...999633, 999665, 999735, 999761, 999850, 999892, 999922]
-#             con.commit()
-#             con.close()
+                    con.execute(f"INSERT INTO first ('timestamp', 'json_server') VALUES (?, ?)", [str({i[0]}), jsonpickle.encode({i[3]})])
+    # #1612068364,76,38152c64-d439-43cf-9856-4ca35d39d7ac,"{""purpose"": ""...
+    #         # result = cur.execute("select * from cheaters")
+    #         # data_from_cheaters = [i for i in result]
+    #         #############################################################
+    #         # data_from_cheaters_0 = [i[0] for i in result]
+    #         # print('len(data_from_cheaters)', len(data_from_cheaters))
+    #         # cur.execute("CREATE TABLE first(timestamp, player_id, event_id, error_id, json_server, json_client, year, score)")
+    #         # list(cur.fetchall())
+    #         # print(data_from_cheaters)#[...(999892, '2021-01-25 14:38:15'), (999922, '2021-05-18 03:50:08')]
+    #         # print(data_from_cheaters_0)#[...999633, 999665, 999735, 999761, 999850, 999892, 999922]
+                con.commit()
+        con.close()
